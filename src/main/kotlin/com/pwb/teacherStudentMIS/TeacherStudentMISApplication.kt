6ab7cf797567f93a7647f3940a75f43ff84fbfda
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 03:42:01 +0100

    Initial commit for TeacherStudentMIS

---
+++ src/main/kotlin/com/pwb/teacherStudentMIS/TeacherStudentMISApplication.kt
@@ -0,0 +1,13 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T02:40:52.517852900
+package com.pwb.teacherStudentMIS
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeacherStudentMISApplication
+
+fun main(args: Array<String>) {
+	runApplication< TeacherStudentMISApplication>(*args)
+}
+
