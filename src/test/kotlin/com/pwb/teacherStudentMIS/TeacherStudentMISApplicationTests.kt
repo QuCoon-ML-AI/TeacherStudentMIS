e2ece0393aa0840de80863f0af6791144c1cb6a6
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 03:42:10 +0100

    Initial commit for TeacherStudentMIS

---
+++ src/test/kotlin/com/pwb/teacherStudentMIS/TeacherStudentMISApplicationTests.kt
@@ -0,0 +1,15 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T02:40:52.549103800
+package com.pwb.teacherStudentMIS
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeacherStudentMISApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
