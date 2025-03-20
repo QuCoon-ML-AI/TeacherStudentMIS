0f7caa478f5f90a99d065d0bfe8223272dc08253
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 03:42:03 +0100

    Initial commit for TeacherStudentMIS

---
+++ src/main/kotlin/com/pwb/teacherStudentMIS/utility/constant/PrivilegeConstant.kt
@@ -0,0 +1,11 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T02:40:52.517852900
+package com.pwb.teacherStudentMIS.utility.constant
+
+object PrivilegeConstant {
+    const val CREATE = "CREATE"
+    const val UPDATE = "UPDATE"
+    const val READ = "READ"
+    const val DELETE = "DELETE"
+    const val CHECKER = "CHECKER"
+    const val ALL = "*"
+}
