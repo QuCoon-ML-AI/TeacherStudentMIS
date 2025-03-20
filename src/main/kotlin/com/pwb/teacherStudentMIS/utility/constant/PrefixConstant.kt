9a9d4ddfba2512b056a2bd88ddabd02c14c34be1
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 03:42:02 +0100

    Initial commit for TeacherStudentMIS

---
+++ src/main/kotlin/com/pwb/teacherStudentMIS/utility/constant/PrefixConstant.kt
@@ -0,0 +1,6 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T02:40:52.517852900
+package com.pwb.teacherStudentMIS.utility.constant
+
+object PrefixConstant {
+    const val ENTITY = "DBO."
+}
